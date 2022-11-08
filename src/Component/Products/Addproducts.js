f57cbbba54a48d1bd5224ea47a54d6eac4496b5e
elitewebsite <eliteele11@gmail.com>
2022-11-08 14:54:32 +0530

    final elite frontend

--- src/Component/Products/Addproducts.js
+++ src/Component/Products/Addproducts.js
@@ -188,7 +188,7 @@ const Addproducts = () => {
 
                     <div className="w-full mt-4">
                       <label htmlFor="pdffile"><b>Downloadable pdf Data:</b></label>
-                      <input type="file" name='pdffile' placeholder="PDF" className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" required/>
+                      <input type="file" name='pdffile' placeholder="PDF" className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-md dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" required />
                     </div>
 
                     <div className="w-full mt-4">
